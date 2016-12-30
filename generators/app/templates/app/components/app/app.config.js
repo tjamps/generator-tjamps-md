135a62d62cbb19e9df1dda508b0afadbe4fc6627
Rémi <remi.tjampens@gmail.com>
2016-12-30 23:55:37 +0100

    Configure $logProvider
    - Disable debug messages in production

--- generators/app/templates/app/components/app/app.config.js
+++ generators/app/templates/app/components/app/app.config.js
@@ -3,7 +3,8 @@
 
   angular
     .module('app')
-    .config(['$compileProvider', 'APP_PARAMS', function($compileProvider, APP_PARAMS) {
+    .config(['$compileProvider', '$logProvider', 'APP_PARAMS', function($compileProvider, $logProvider, APP_PARAMS) {
       $compileProvider.debugInfoEnabled(APP_PARAMS.debug === '1' ? true : false);
+      $logProvider.debugEnabled(APP_PARAMS.debug === '1' ? true : false);
     }]);
 })();
